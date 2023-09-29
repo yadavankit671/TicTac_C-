628a006886997a2080fb7ceaaa7bc426a9838d4e
Ankit Yadav <142900873+yadavankit671@users.noreply.github.com>
2023-09-30 01:46:41 +0530

    Add project files.

---
+++ TicTac/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace TicTac
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicTacToe));
+            this.button_1x1 = new System.Windows.Forms.Button();
+            this.button_1x2 = new System.Windows.Forms.Button();
+            this.button_1x3 = new System.Windows.Forms.Button();
+            this.button_2x1 = new System.Windows.Forms.Button();
+            this.button_2x2 = new System.Windows.Forms.Button();
+            this.button_2x3 = new System.Windows.Forms.Button();
+            this.button_3x1 = new System.Windows.Forms.Button();
+            this.button_3x2 = new System.Windows.Forms.Button();
+            this.button_3x3 = new System.Windows.Forms.Button();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_1x1
+            // 
+            this.button_1x1.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_1x1.Location = new System.Drawing.Point(21, 109);
+            this.button_1x1.Name = "button_1x1";
+            this.button_1x1.Size = new System.Drawing.Size(100, 100);
+            this.button_1x1.TabIndex = 0;
+            this.button_1x1.Text = " ";
+            this.button_1x1.UseVisualStyleBackColor = true;
+            this.button_1x1.Click += new System.EventHandler(this.play_input);
+            // 
+            // button_1x2
+            // 
+            this.button_1x2.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_1x2.Location = new System.Drawing.Point(127, 109);
+            this.button_1x2.Name = "button_1x2";
+            this.button_1x2.Size = new System.Drawing.Size(100, 100);
+            this.button_1x2.TabIndex = 0;
+            this.button_1x2.Text = " ";
+            this.button_1x2.UseVisualStyleBackColor = true;
+            this.button_1x2.Click += new System.EventHandler(this.play_input);
+            // 
+            // button_1x3
+            // 
+            this.button_1x3.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_1x3.Location = new System.Drawing.Point(233, 109);
+            this.button_1x3.Name = "button_1x3";
+            this.button_1x3.Size = new System.Drawing.Size(100, 100);
+            this.button_1x3.TabIndex = 0;
+            this.button_1x3.Text = " ";
+            this.button_1x3.UseVisualStyleBackColor = true;
+            this.button_1x3.Click += new System.EventHandler(this.play_input);
+            // 
+            // button_2x1
+            // 
+            this.button_2x1.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_2x1.Location = new System.Drawing.Point(21, 215);
+            this.button_2x1.Name = "button_2x1";
+            this.button_2x1.Size = new System.Drawing.Size(100, 100);
+            this.button_2x1.TabIndex = 0;
+            this.button_2x1.Text = " ";
+            this.button_2x1.UseVisualStyleBackColor = true;
+            this.button_2x1.Click += new System.EventHandler(this.play_input);
+            // 
+            // button_2x2
+            // 
+            this.button_2x2.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_2x2.Location = new System.Drawing.Point(127, 215);
+            this.button_2x2.Name = "button_2x2";
+            this.button_2x2.Size = new System.Drawing.Size(100, 100);
+            this.button_2x2.TabIndex = 0;
+            this.button_2x2.Text = " ";
+            this.button_2x2.UseVisualStyleBackColor = true;
+            this.button_2x2.Click += new System.EventHandler(this.play_input);
+            // 
+            // button_2x3
+            // 
+            this.button_2x3.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_2x3.Location = new System.Drawing.Point(233, 215);
+            this.button_2x3.Name = "button_2x3";
+            this.button_2x3.Size = new System.Drawing.Size(100, 100);
+            this.button_2x3.TabIndex = 0;
+            this.button_2x3.Text = " ";
+            this.button_2x3.UseVisualStyleBackColor = true;
+            this.button_2x3.Click += new System.EventHandler(this.play_input);
+            // 
+            // button_3x1
+            // 
+            this.button_3x1.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_3x1.Location = new System.Drawing.Point(21, 321);
+            this.button_3x1.Name = "button_3x1";
+            this.button_3x1.Size = new System.Drawing.Size(100, 100);
+            this.button_3x1.TabIndex = 0;
+            this.button_3x1.Text = " ";
+            this.button_3x1.UseVisualStyleBackColor = true;
+            this.button_3x1.Click += new System.EventHandler(this.play_input);
+            // 
+            // button_3x2
+            // 
+            this.button_3x2.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_3x2.Location = new System.Drawing.Point(127, 321);
+            this.button_3x2.Name = "button_3x2";
+            this.button_3x2.Size = new System.Drawing.Size(100, 100);
+            this.button_3x2.TabIndex = 0;
+            this.button_3x2.Text = " ";
+            this.button_3x2.UseVisualStyleBackColor = true;
+            this.button_3x2.Click += new System.EventHandler(this.play_input);
+            // 
+            // button_3x3
+            // 
+            this.button_3x3.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_3x3.Location = new System.Drawing.Point(233, 321);
+            this.button_3x3.Name = "button_3x3";
+            this.button_3x3.Size = new System.Drawing.Size(100, 100);
+            this.button_3x3.TabIndex = 0;
+            this.button_3x3.Text = " ";
+            this.button_3x3.UseVisualStyleBackColor = true;
+            this.button_3x3.Click += new System.EventHandler(this.play_input);
+            // 
+            // button_exit
+            // 
+            this.button_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_exit.Location = new System.Drawing.Point(350, 269);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(100, 152);
+            this.button_exit.TabIndex = 0;
+            this.button_exit.Text = "EXIT";
+            this.button_exit.UseVisualStyleBackColor = true;
+            this.button_exit.Click += new System.EventHandler(this.exit);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_clear.Location = new System.Drawing.Point(350, 109);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(100, 152);
+            this.button_clear.TabIndex = 0;
+            this.button_clear.Text = "CLEAR";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.input_clear);
+            // 
+            // textbox
+            // 
+            this.textbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(135)))), ((int)(((byte)(67)))));
+            this.textbox.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbox.Location = new System.Drawing.Point(21, 12);
+            this.textbox.Multiline = true;
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(429, 91);
+            this.textbox.TabIndex = 1;
+            this.textbox.Text = "LET\'S PLAY";
+            this.textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.no_keypress);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(150)))), ((int)(((byte)(190)))));
+            this.ClientSize = new System.Drawing.Size(462, 433);
+            this.Controls.Add(this.textbox);
+            this.Controls.Add(this.button_3x3);
+            this.Controls.Add(this.button_2x3);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.button_1x3);
+            this.Controls.Add(this.button_3x2);
+            this.Controls.Add(this.button_2x2);
+            this.Controls.Add(this.button_1x2);
+            this.Controls.Add(this.button_3x1);
+            this.Controls.Add(this.button_2x1);
+            this.Controls.Add(this.button_1x1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TicTacToe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_1x1;
+        private System.Windows.Forms.Button button_1x2;
+        private System.Windows.Forms.Button button_1x3;
+        private System.Windows.Forms.Button button_2x1;
+        private System.Windows.Forms.Button button_2x2;
+        private System.Windows.Forms.Button button_2x3;
+        private System.Windows.Forms.Button button_3x1;
+        private System.Windows.Forms.Button button_3x2;
+        private System.Windows.Forms.Button button_3x3;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.TextBox textbox;
+    }
+}
+
---
+++ TicTac/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTac
+{
+    public partial class TicTacToe : Form
+    {
+        public bool player = true;
+        public bool no_play = false;
+        public int count = 0;
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void exit(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void play_input(object sender, EventArgs e)
+        {
+            Button clicked =sender as Button;
+            if (clicked.Text==" " && no_play == false)
+            {
+                clicked.Text = (player == true) ? "O" : "X";
+                player = (player == true) ? false : true;
+                count++;
+                check();
+            }
+            if (count>8 && no_play == false)
+            {
+                no_play = true;
+                textbox.Text = "GAME OVER !!";
+            }
+        }
+        
+         
+         public void check() {
+            if (button_1x1.Text == button_1x2.Text && button_1x1.Text == button_1x3.Text && !button_1x1.Text.Contains(" "))
+            {
+                textbox.Text = (player == true) ? "Winner P2" : "Winner P1";
+                no_play = true;
+            }
+            else if (button_2x1.Text == button_2x2.Text && button_2x2.Text == button_2x3.Text && !button_2x1.Text.Contains(" "))
+            {
+                textbox.Text = (player == true) ? "Winner P2" : "Winner P1";
+                no_play = true;
+            }
+            else if (button_3x1.Text == button_3x2.Text && button_3x2.Text == button_3x3.Text && !button_3x1.Text.Contains(" "))
+            {
+                textbox.Text = (player == true) ? "Winner P2" : "Winner P1";
+                no_play = true;
+            }
+            else if (button_1x1.Text == button_2x1.Text && button_2x1.Text == button_3x1.Text && !button_1x1.Text.Contains(" "))
+            {
+                textbox.Text = (player == true) ? "Winner P2" : "Winner P1";
+                no_play = true;
+            }
+            else if (button_1x2.Text == button_2x2.Text && button_2x2.Text == button_3x2.Text && !button_1x2.Text.Contains(" "))
+            {
+                textbox.Text = (player == true) ? "Winner P2" : "Winner P1";
+                no_play = true;
+            }
+            else if (button_1x3.Text == button_2x3.Text && button_2x3.Text == button_3x3.Text && !button_1x3.Text.Contains(" "))
+            {
+                textbox.Text = (player == true) ? "Winner P2" : "Winner P1";
+                no_play = true;
+            }
+            else if (button_1x1.Text == button_2x2.Text && button_2x2.Text == button_3x3.Text && !button_1x1.Text.Contains(" "))
+            {
+                textbox.Text = (player == true) ? "Winner P2" : "Winner P1";
+                no_play = true;
+            }
+            else if (button_1x3.Text == button_2x2.Text && button_2x2.Text == button_3x1.Text && !button_1x3.Text.Contains(" "))
+            {
+                textbox.Text = (player == true) ? "Winner P2" : "Winner P1";
+                no_play = true;
+            }
+        }
+         
+
+
+        private void input_clear(object sender, EventArgs e)
+        {
+            button_1x1.Text = " ";
+            button_1x2.Text = " ";
+            button_1x3.Text = " ";
+            button_2x1.Text = " ";
+            button_2x2.Text = " ";
+            button_2x3.Text = " ";
+            button_3x1.Text = " ";
+            button_3x2.Text = " ";
+            button_3x3.Text = " ";
+            textbox.Text = "LET'S PLAY";
+            player = true;
+            count= 0;
+            no_play= false;
+        }
+
+        private void no_keypress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
